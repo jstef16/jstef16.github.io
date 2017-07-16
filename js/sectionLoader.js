3596299e6662194eb20d9117b1832b84b7a37733
jstef16 <jsteffan710@gmail.com>
2017-07-16 00:51:14 -0500

    Load Methods
    Updated header to use Load javascript instead of HTML redirects

--- js/sectionLoader.js
+++ js/sectionLoader.js
@@ -1,7 +1,18 @@
       function LoadHeader(){
           $("#header").load("../pages/header.html");
       }
-
       function LoadAbout(){
           $("#content").load("../pages/about.html");
+      }
+      function LoadBlog(){
+          $("#content").load("../pages/blog.html");
+      }
+      function LoadContact(){
+          $("#content").load("../pages/contact.html");
+      }
+      function LoadProjects(){
+          $("#content").load("../pages/projects.html");
+      }
+      function LoadWorkExperience(){
+          $("#content").load("../pages/workexperience.html");
       }
