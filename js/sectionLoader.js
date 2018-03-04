bc6b26487f9b5fa1a075446f22f95e4ce11791e8
jstef16 <jsteffan710@gmail.com>
2018-03-04 17:37:22 -0600

    Trying to set active tab coloring

--- js/sectionLoader.js
+++ js/sectionLoader.js
@@ -1,18 +1,34 @@
       function LoadHeader(){
-          $("#header").load("../pages/header.html");
+        $("#header").load("../pages/header.html");
       }
+
       function LoadAbout(){
-          $("#content").load("../pages/about.html");
+        ("#content").load("../pages/about.html");
+        SetActiveTab("about");
       }
+
       function LoadBlog(){
-          $("#content").load("../pages/blog.html");
+        $("#content").load("../pages/blog.html");
+        SetActiveTab("blog");
       }
+
       function LoadContact(){
-          $("#content").load("../pages/contact.html");
+        $("#content").load("../pages/contact.html");
+        SetActiveTab("contact");
       }
+
       function LoadProjects(){
-          $("#content").load("../pages/projects.html");
+        $("#content").load("../pages/projects.html");
+        SetActiveTab("projects");
       }
+      
       function LoadWorkExperience(){
-          $("#content").load("../pages/workexperience.html");
+        $("#content").load("../pages/workexperience.html");
+        SetActiveTab("experience")
+      }
+
+      function SetActiveTab(tabName){
+        var tabs = document.getElementsByClassName("tab");
+        tabs.each($(this).removeClass("active"));
+        document.getElementById(tabName).addClass("active");
       }
